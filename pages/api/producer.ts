370d8672e4e737b079ce68e9740e6eefe7117930
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2022-05-01 10:43:52 +0200

    Add producer init

--- pages/api/producer.ts
+++ pages/api/producer.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import fetch from "node-fetch";
 
+const SERVERLESSQ_QUEUE_ID = process.env.SERVERLESSQ_QUEUE_ID;
+const SERVERLESSQ_API_TOKEN = process.env.SERVERLESSQ_API_TOKEN;
+const VERCEL_URL = process.env.VERCEL_URL;
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const targetUrl = `https://${VERCEL_URL}/api/consumer?responseCode=200`;
+  console.log("Target URL: ", targetUrl);
+
+  await fetch(
+    `https://api.serverlessq.com?id=${SERVERLESSQ_QUEUE_ID}&target=${targetUrl}`,
+    { headers: { "x-api-key": SERVERLESSQ_API_TOKEN! } }
+  );
+
+  res.status(200).json({ name: "Message sent to queue" });
 }
